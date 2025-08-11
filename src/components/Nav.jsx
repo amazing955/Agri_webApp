090859466adba466f3f65f4359ac0e494844443e
Batubo Victory <victorybatubo76@gmail.com>
2025-08-11 13:49:31 +0100

    Add nav content

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,23 @@
 function Nav(){
   return(
-    <nav>
-      
+    <nav className="flex align-center justify-around flex-wrap">
+      <p className="font-bold text-green-600 text-xl">
+        AgriSacco
+      </p>
+
+      <ul className="flex justify-around gap-1 align-center flex-wrap">
+        <li>Home</li>
+        <li>About</li>
+        <li>MarketPlace</li>
+        <li>Resources</li>
+        <li>Contact</li>
+        <li>Weather</li>
+      </ul>
+
+      <article className="flex justify-between gap-2">
+        <button className="text-white bg-green-600 py-1 px-2 rounded-full">LogIn</button>
+        <button className="text-white bg-green-600 py-1 px-2 rounded-full">SignUp</button>
+      </article>
     </nav>
   )
 }
