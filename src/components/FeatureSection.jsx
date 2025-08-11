e3247d886e76e3c8da59b0fcbbb8e4f6513e6943
Batubo Victory <victorybatubo76@gmail.com>
2025-08-11 15:44:40 +0100

    Create FeatureSection component

---
+++ src/components/FeatureSection.jsx
@@ -0,0 +1,14 @@
+function FeatureSection(){
+  return(
+    <section>
+      <h2 className="text-center text-3xl font-bold my-3">
+        Key Features
+      </h2>
+      <p className="text-center text-gray-700">
+        Everything you need to succeed in agriculture
+      </p>
+    </section>
+  )
+}
+
+export default FeatureSection
