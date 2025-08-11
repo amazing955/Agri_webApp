77a3edc286d5bf06f0077d273f5e245e64a8184c
Batubo Victory <victorybatubo76@gmail.com>
2025-08-11 15:00:34 +0100

    Import Hero component

--- src/pages/HomePage/Header.jsx
+++ src/pages/HomePage/Header.jsx
@@ -1,9 +1,11 @@
 import Nav from "../../components/Nav";
+import Hero from "../../components/Hero";
 
 function Header(){
   return(
     <section>
       <Nav />
+      <Hero />
     </section>
   )
 }
