e675087f5be54451dc8563ffcf9e6e973ed9c4ae
Batubo Victory <victorybatubo76@gmail.com>
2025-08-11 15:47:42 +0100

    Import FeatureSection component

--- src/pages/HomePage/Body.jsx
+++ src/pages/HomePage/Body.jsx
@@ -1,9 +1,12 @@
 import Mission from "../../components/Mission";
+import FeatureSection from '../../components/FeatureSection';
+
 
 function Body(){
   return (
     <main>
       <Mission />
+      <FeatureSection />
     </main>
   )
 }
