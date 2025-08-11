3342f8c889c34157d602a5c1f4aca869daa4bf95
Batubo Victory <victorybatubo76@gmail.com>
2025-08-11 17:08:11 +0100

    Import cards and add appropriate prop values

--- src/components/StepSection.jsx
+++ src/components/StepSection.jsx
@@ -1,6 +1,8 @@
+import StepsCard from "./StepsCard";
+
 function HowItWorks(){
   return(
-    <section>
+    <section className="p-2">
       <h2 className="text-center text-3xl font-bold my-3">
         How It Works
       </h2>
@@ -8,6 +10,14 @@ function HowItWorks(){
       <p className="text-center text-gray-700">
         Simple steps to get started
       </p>
+
+      <article className="grid grid-cols-2 gap-4 mt-4">
+        <StepsCard cardImage='fas fa-user-plus' cardStep='1. Sign Up' cardDescription='Create your account as a farmer or buyer to get started' />
+
+        <StepsCard cardImage='fas fa-list' cardStep='2. Post or Browse' cardDescription='List your produce or browse available listings' />
+        
+        <StepsCard cardImage='fas fa-handshake' cardStep='3. Connect & Trade' cardDescription='Communicate and complete transactions securely' />
+      </article>
     </section>
   )
 }
