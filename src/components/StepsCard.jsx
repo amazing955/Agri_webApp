02941e1ec99890104e418f85877edccf45826c74
Batubo Victory <victorybatubo76@gmail.com>
2025-08-11 16:41:17 +0100

    Style card components

--- src/components/StepsCard.jsx
+++ src/components/StepsCard.jsx
@@ -1,7 +1,15 @@
-function StepsCard(){
+function StepsCard({ cardImage, cardStep, cardDescription}){
   return(
-    <article>
+    <article className="flex flex-col *:text-center justify-center align-top p-5 bg-gray-100 rounded-xl shadow-md">
+      <i className={`${cardImage} text-green-700 text-[2rem] mb-2`}></i>
 
+      <h3 className="font-semibold text-xl mb-2">
+        {cardStep}
+      </h3>
+
+      <p className="text-gray-700">
+        {cardDescription}
+</p>
     </article>
   )
 }
