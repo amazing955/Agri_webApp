3496790d6469fd7f4d35f3baadce04f0d785121c
Batubo Victory <victorybatubo76@gmail.com>
2025-08-11 16:02:25 +0100

    Add FeaturesCard components and pass their respective props

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -2,7 +2,7 @@ import FeaturesCard from "./FeaturesCard"
 
 function FeatureSection(){
   return(
-    <section>
+    <section className="p-2">
       <h2 className="text-center text-3xl font-bold my-3">
         Key Features
       </h2>
@@ -10,8 +10,22 @@ function FeatureSection(){
         Everything you need to succeed in agriculture
       </p>
 
-      <article className="grid grid-cols-1">
-        <FeaturesCard />
+      <article className="grid grid-cols-2 gap-1">
+        <FeaturesCard cardImage='src\assets\Images\Hero\photo-1500937386664-56d1dfef3854.webp' cardTitle='MarketPlace' cardDescription='Buy and sell farm produce directly without middlemen' />
+
+        <FeaturesCard cardImage='src\assets\Images\Hero\photo-1500937386664-56d1dfef3854.webp' cardTitle='Training Resources' cardDescription='Access educational content on best farming practices' />
+
+        <FeaturesCard cardImage='src\assets\Images\Hero\photo-1500937386664-56d1dfef3854.webp' cardTitle='Price Tracking' cardDescription='Monitor market trends and price analytics' />
+
+        <FeaturesCard cardImage='src\assets\Images\Hero\photo-1500937386664-56d1dfef3854.webp' cardTitle='Farmer Network' cardDescription='Connect with other farmers for knowledge sharing' />
+
+        <FeaturesCard cardImage='src\assets\Images\Hero\photo-1500937386664-56d1dfef3854.webp' cardTitle='Savings Tracker' cardDescription='Manage your finances and track savings goals' />
+
+        <FeaturesCard cardImage='src\assets\Images\Hero\photo-1500937386664-56d1dfef3854.webp' cardTitle='Weather Forecast' cardDescription='Get accurate weather predictions for better planning' />
+
+        <FeaturesCard cardImage='src\assets\Images\Hero\photo-1500937386664-56d1dfef3854.webp' cardTitle='Machine Hire' cardDescription='Access affordable farm equipment when you need it' />
+
+        <FeaturesCard cardImage='src\assets\Images\Hero\photo-1500937386664-56d1dfef3854.webp' cardTitle='Labor Connection' cardDescription='Find and hire skilled farm workers in your area' />
       </article>
     </section>
   )
