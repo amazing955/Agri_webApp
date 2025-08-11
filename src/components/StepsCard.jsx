fadc38d622a8872197d9ba5fafaf735e8dee5811
Batubo Victory <victorybatubo76@gmail.com>
2025-08-11 17:08:51 +0100

    Style fa icon

--- src/components/StepsCard.jsx
+++ src/components/StepsCard.jsx
@@ -1,7 +1,9 @@
 function StepsCard({ cardImage, cardStep, cardDescription}){
   return(
     <article className="flex flex-col *:text-center justify-center align-top p-5 bg-gray-100 rounded-xl shadow-md">
-      <i className={`${cardImage} text-green-700 text-[2rem] mb-2`}></i>
+      <div>
+        <i className={`${cardImage} text-green-700 text-[2rem] mb-2`}></i>
+      </div>
 
       <h3 className="font-semibold text-xl mb-2">
         {cardStep}
