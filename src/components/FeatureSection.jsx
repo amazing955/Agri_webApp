398f337dca92b15dc187ee3c1dc8e4db5b197a68
Batubo Victory <victorybatubo76@gmail.com>
2025-08-14 13:45:51 +0100

    Indent code for better readability

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -3,14 +3,17 @@ import FeaturesCard from "./FeaturesCard"
 function FeatureSection(){
   return(
     <section className="p-2">
+
       <h2 className="text-center text-3xl font-bold my-3">
         Key Features
       </h2>
+
       <p className="text-center text-gray-700">
         Everything you need to succeed in agriculture
       </p>
 
       <article className="grid grid-cols-2 gap-4 mt-4">
+
         <FeaturesCard cardImage='fas fa-shopping-cart' cardTitle='MarketPlace' cardDescription='Buy and sell farm produce directly without middlemen' />
 
         <FeaturesCard cardImage='fas fa-book-open' cardTitle='Training Resources' cardDescription='Access educational content on best farming practices' />
@@ -26,7 +29,9 @@ function FeatureSection(){
         <FeaturesCard cardImage='fas fa-truck' cardTitle='Machine Hire' cardDescription='Access affordable farm equipment when you need it' />
 
         <FeaturesCard cardImage='fas fa-user-friends' cardTitle='Labor Connection' cardDescription='Find and hire skilled farm workers in your area' />
+
       </article>
+      
     </section>
   )
 }
