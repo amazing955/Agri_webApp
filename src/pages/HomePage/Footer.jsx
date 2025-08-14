5da1d97f8112b5536f3d5552ba5bc7c49cda6a48
Batubo Victory <victorybatubo76@gmail.com>
2025-08-14 12:32:34 +0100

    Add padding to all sides

--- src/pages/HomePage/Footer.jsx
+++ src/pages/HomePage/Footer.jsx
@@ -2,7 +2,7 @@ import FooterHeading from "../../components/FooterHeading";
 
 function Footer(){
   return (
-    <footer className='w-full bg-[#19192c] text-white'>
+    <footer className='w-full bg-[#19192c] text-white p-4'>
       <FooterHeading />
     </footer>
   )
