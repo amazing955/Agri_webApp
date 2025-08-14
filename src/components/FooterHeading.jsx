4fe6e28bd7397ca7f605ee119d5850853488bf01
Batubo Victory <victorybatubo76@gmail.com>
2025-08-14 12:34:25 +0100

    Style content

--- src/components/FooterHeading.jsx
+++ src/components/FooterHeading.jsx
@@ -1,14 +1,14 @@
 function FooterHeading(){
   return(
-    <>
-      <h4>
+    <article className="flex flex-col">
+      <h4 className="font-bold mb-2">
         AgriSacco
       </h4>
 
       <p>
         Empowering farmers throught technology and community
       </p>
-    </>
+    </article>
   )
 }
 
