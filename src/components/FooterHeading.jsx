dcc0c7273620c4d86dfe0acc4ca758324d947d10
Batubo Victory <victorybatubo76@gmail.com>
2025-08-14 12:29:51 +0100

    Add FooterHeading content

---
+++ src/components/FooterHeading.jsx
@@ -0,0 +1,15 @@
+function FooterHeading(){
+  return(
+    <>
+      <h4>
+        AgriSacco
+      </h4>
+
+      <p>
+        Empowering farmers throught technology and community
+      </p>
+    </>
+  )
+}
+
+export default FooterHeading;
