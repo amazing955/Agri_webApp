9c79fbdb6965c8a04862a4c0388d85a3cadb2c2f
Batubo Victory <victorybatubo76@gmail.com>
2025-08-14 12:40:59 +0100

    Import FooterLinks component and change footer display to flex

--- src/pages/HomePage/Footer.jsx
+++ src/pages/HomePage/Footer.jsx
@@ -1,9 +1,11 @@
 import FooterHeading from "../../components/FooterHeading";
+import FooterLinks from "../../components/FooterLinks";
 
 function Footer(){
   return (
-    <footer className='w-full bg-[#19192c] text-white p-4'>
+    <footer className='w-full bg-[#19192c] text-white p-4 flex flex-col gap-8'>
       <FooterHeading />
+      <FooterLinks />
     </footer>
   )
 }
