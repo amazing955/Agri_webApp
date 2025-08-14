cc1b38d4dcc1c317b3f7e99ce9de819031bc4a49
Batubo Victory <victorybatubo76@gmail.com>
2025-08-14 13:48:06 +0100

    Make code more readable

--- src/components/FooterLinks.jsx
+++ src/components/FooterLinks.jsx
@@ -1,18 +1,22 @@
 function FooterLinks(){
   return(
     <article>
+      
       <h4 className="font-bold mb-2 text-xl">
         Quick Links
       </h4>
 
       <nav>
+
         <ul>
           <li>Home</li>
           <li>About</li>
           <li>Features</li>
           <li>Contact</li>
         </ul>
+
       </nav>
+      
     </article>
   )
 }
