26cbe1d7f49f4efab3317dad2124ec0feb057e2d
Batubo Victory <victorybatubo76@gmail.com>
2025-08-14 12:50:56 +0100

    Create FooterSocial content

---
+++ src/components/FooterSocials.jsx
@@ -0,0 +1,20 @@
+function FooterSocials(){
+  return(
+    <article>
+      <h4 className="font-bold mb-2 text-xl">
+        Connect
+      </h4>
+
+      <nav>
+        <ul>
+          <li><a href="">Github</a></li>
+          <li><a href="">Twitter</a></li>
+          <li><a href="">Facebook</a></li>
+          <li><a href="">Instagram</a></li>
+        </ul>
+      </nav>
+    </article>
+  )
+}
+
+export default FooterSocials;
