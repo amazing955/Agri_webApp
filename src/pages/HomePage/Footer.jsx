6ced208a8d610a9098c075078fc7b7f4c81d1cf1
Batubo Victory <victorybatubo76@gmail.com>
2025-08-14 12:45:40 +0100

    Import FooterResources component

--- src/pages/HomePage/Footer.jsx
+++ src/pages/HomePage/Footer.jsx
@@ -1,11 +1,13 @@
 import FooterHeading from "../../components/FooterHeading";
 import FooterLinks from "../../components/FooterLinks";
+import FooterResources from "../../components/FooterResources";
 
 function Footer(){
   return (
     <footer className='w-full bg-[#19192c] text-white p-4 flex flex-col gap-8'>
       <FooterHeading />
       <FooterLinks />
+      <FooterResources />
     </footer>
   )
 }
