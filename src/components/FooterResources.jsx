f8993d63e6bdac9190d168bc10657b29ad4814b7
Batubo Victory <victorybatubo76@gmail.com>
2025-08-14 12:47:55 +0100

    Create content

---
+++ src/components/FooterResources.jsx
@@ -0,0 +1,20 @@
+function FooterResources(){
+  return(
+    <article>
+      <h4 className="font-bold mb-2 text-xl">
+        Resources
+      </h4>
+
+      <nav>
+        <ul>
+          <li>Training</li>
+          <li>Weather</li>
+          <li>Market Prices</li>
+          <li>FAQ</li>
+        </ul>
+      </nav>
+    </article>
+  )
+}
+
+export default FooterResources;
