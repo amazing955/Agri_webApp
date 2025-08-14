716be49aa4127384c8411b3a47abfc07951147e3
Batubo Victory <victorybatubo76@gmail.com>
2025-08-14 13:49:31 +0100

    Make code more readable

--- src/components/FooterSocials.jsx
+++ src/components/FooterSocials.jsx
@@ -6,12 +6,25 @@ function FooterSocials(){
       </h4>
 
       <nav>
+
         <ul>
-          <li><a href="">Github</a></li>
-          <li><a href="">Twitter</a></li>
-          <li><a href="">Facebook</a></li>
-          <li><a href="">Instagram</a></li>
+          <li>
+            <a href="">Github</a>
+          </li>
+
+          <li>
+            <a href="">Twitter</a>
+          </li>
+
+          <li>
+            <a href="">Facebook</a>
+          </li>
+          
+          <li>
+            <a href="">Instagram</a>
+          </li>
         </ul>
+
       </nav>
     </article>
   )
